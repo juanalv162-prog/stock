ff73cfb7671a9634733aae1d0d5b68fecd561917
pollo <pollo@COMPUJUAN>
2026-09-06 21:19:08 -0300

    Agregar archivos de proyecto.

---
+++ stock bebidas/Form1.Designer.cs
@@ -0,0 +1,133 @@
+﻿namespace stock_bebidas
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtbebidas = new System.Windows.Forms.TextBox();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.btnAgregar = new System.Windows.Forms.Button();
+            this.btnMostrar = new System.Windows.Forms.Button();
+            this.IstStock = new System.Windows.Forms.ListBox();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(124, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Nombre de bebida:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 66);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(64, 16);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Cantidad:";
+            // 
+            // txtbebidas
+            // 
+            this.txtbebidas.Location = new System.Drawing.Point(16, 32);
+            this.txtbebidas.Name = "txtbebidas";
+            this.txtbebidas.Size = new System.Drawing.Size(100, 22);
+            this.txtbebidas.TabIndex = 2;
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(12, 95);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(120, 22);
+            this.numericUpDown1.TabIndex = 3;
+            // 
+            // btnAgregar
+            // 
+            this.btnAgregar.Location = new System.Drawing.Point(16, 137);
+            this.btnAgregar.Name = "btnAgregar";
+            this.btnAgregar.Size = new System.Drawing.Size(221, 23);
+            this.btnAgregar.TabIndex = 4;
+            this.btnAgregar.Text = "AGREGAR BEBIDAS";
+            this.btnAgregar.UseVisualStyleBackColor = true;
+            // 
+            // btnMostrar
+            // 
+            this.btnMostrar.Location = new System.Drawing.Point(12, 166);
+            this.btnMostrar.Name = "btnMostrar";
+            this.btnMostrar.Size = new System.Drawing.Size(167, 23);
+            this.btnMostrar.TabIndex = 5;
+            this.btnMostrar.Text = "MOSTRAR STOCK";
+            this.btnMostrar.UseVisualStyleBackColor = true;
+            this.btnMostrar.Click += new System.EventHandler(this.btnMostrar_Click);
+            // 
+            // IstStock
+            // 
+            this.IstStock.FormattingEnabled = true;
+            this.IstStock.ItemHeight = 16;
+            this.IstStock.Location = new System.Drawing.Point(69, 209);
+            this.IstStock.Name = "IstStock";
+            this.IstStock.Size = new System.Drawing.Size(513, 84);
+            this.IstStock.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.IstStock);
+            this.Controls.Add(this.btnMostrar);
+            this.Controls.Add(this.btnAgregar);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.txtbebidas);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtbebidas;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Button btnAgregar;
+        private System.Windows.Forms.Button btnMostrar;
+        private System.Windows.Forms.ListBox IstStock;
+    }
+}
+
---
+++ stock bebidas/Form1.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+
+namespace stock_bebidas
+{
+    public partial class Form1 : Form
+    {
+        // Lista para guardar las bebidas
+        List<string> bebidas = new List<string>();
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            // Conectar el botón AGREGAR con el código
+            btnAgregar.Click += btnAgregar_Click;
+        }
+
+        // BOTÓN AGREGAR BEBIDAS
+        private void btnAgregar_Click(object sender, EventArgs e)
+        {
+            string nombre = txtbebidas.Text;
+            int cantidad = (int)numericUpDown1.Value;
+
+            // Comprobar que se haya escrito el nombre
+            if (nombre == "")
+            {
+                MessageBox.Show("Ingrese el nombre de la bebida.");
+                return;
+            }
+
+            // Comprobar el stock
+            if (cantidad < 3)
+            {
+                bebidas.Add(nombre + " - " + cantidad +
+                    " unidades - STOCK BAJO");
+            }
+            else
+            {
+                bebidas.Add(nombre + " - " + cantidad +
+                    " unidades - STOCK SUFICIENTE");
+            }
+
+            MessageBox.Show("Bebida agregada correctamente.");
+
+            // Limpiar los campos
+            txtbebidas.Clear();
+            numericUpDown1.Value = 0;
+        }
+
+        // BOTÓN MOSTRAR STOCK
+        private void btnMostrar_Click(object sender, EventArgs e)
+        {
+            IstStock.Items.Clear();
+
+            // Recorrer todas las bebidas
+            for (int i = 0; i < bebidas.Count; i++)
+            {
+                IstStock.Items.Add(bebidas[i]);
+            }
+        }
+
+        // CARGA DEL FORMULARIO
+        private void Form1_Load(object sender, EventArgs e)
+        {
+        }
+    }
+}
